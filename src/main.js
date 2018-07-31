4a1f3dcb42afe792b49fee55aef043ad3621ca95
Navjot Kaur <navjotkaur@Navjots-MacBook-Air.local>
2018-07-31 16:02:25 +0530

    updated

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import swal from 'sweetalert';
 
 class Main extends React.Component {
   constructor(props) {
@@ -7,6 +8,7 @@ class Main extends React.Component {
 
     this.state = {
       checked: false,
+      labelsSelected: 0,
       labels: [ {checked: false, value: ''} ]
     }
 
@@ -28,21 +30,25 @@ class Main extends React.Component {
   removeLabel(index) {
     return function() {
       let labels = this.state.labels;
-      console.log(index, labels)
-      labels.splice(index, 1);
-      console.log(labels)
-      this.setState({labels})
+      if (!labels[index].checked) {
+        swal('Please select', 'Please select the label to remove', 'error');
+      } else {
+        labels.splice(index, 1);
+        this.setState({labels, labelsSelected: this.state.labelsSelected - 1})
+      }
     }.bind(this);
   }
 
   globalRemove() {
     let labels = this.state.labels;
+    let labelsSelected = this.state.labelsSelected;
     for (var i = labels.length - 1; i >= 0; i--) {
       if (labels[i].checked) {
         labels.splice(i, 1);
+        labelsSelected = labelsSelected - 1;
       }
     }
-    this.setState({labels});
+    this.setState({labels, labelsSelected});
   }
 
   changeLabel(index) {
@@ -59,14 +65,16 @@ class Main extends React.Component {
     for (var i = 0; i < labels.length; i++) {
       labels[i].checked = !this.state.checked;
     }
-    this.setState({checked: !this.state.checked, labels});
+    let labelsSelected = this.state.checked ? 0 : labels.length;
+    this.setState({checked: !this.state.checked, labels, labelsSelected});
   }
 
   toggleCheck(index) {
     return function(event) {
       let labels = this.state.labels;
       labels[index].checked = !labels[index].checked;
-      this.setState({labels});
+      let labelsSelected = labels[index].checked ? this.state.labelsSelected + 1 : this.state.labelsSelected - 1;
+      this.setState({labels, labelsSelected});
     }.bind(this);
   }
 
@@ -79,9 +87,9 @@ class Main extends React.Component {
           <div onClick={this.toggleCheck} >
             <div style={{paddingTop: 30, height: 40}} className="h-margin">
              <input className="check" type="checkbox" checked={this.state.checked} onChange={this.toggleGlobalCheck.bind(this)}/>
-              <span style={{marginTop: 5}} className= "section-heading"> Section Heading </span>
+              <span style={{marginTop: 5}} className= "section-heading">Section Heading</span>
               <button className="float-right button" onClick={this.globalRemove.bind(this)} >REMOVE</button>
-              <span className= "selected-labels float-right"> 1 Labels Selected </span>
+              {this.state.labelsSelected ? <span className= "selected-labels float-right"> {this.state.labelsSelected} Labels Selected </span> : null}
             </div>
           </div>
 
@@ -96,9 +104,9 @@ class Main extends React.Component {
           {
             labels.map((label, index) => (
                 <div key={index} >
-                  <div style={{marginTop: 20, paddingTop: 30, height: 40}} className="h-margin">
+                  <div style={{marginTop: 40, paddingTop: 30, height: 40}} className="h-margin">
                     <div className="label-text">Label {index + 2}</div>
-                    <div>
+                    <div className='max-width'>
                       <input className="check" type="checkbox" checked={label.checked} onChange={this.toggleCheck(index)}/>
                       <input className="label" value={label.value} onChange={this.changeLabel(index)} />
                       <div onClick={this.removeLabel(index)} className="remove">REMOVE</div>
@@ -110,7 +118,7 @@ class Main extends React.Component {
 
           <hr/>
 
-          <div onClick={this.addLabel} style={{width: 600, marginLeft: 80, marginTop: 20}}>
+          <div onClick={this.addLabel} className="add-label-container">
             <p className="add-label"> + ADD LABEL</p>
           </div>
 
